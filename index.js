c9afd2eebf83bd6e8e3697cc9b7494e57169fa18
unknown <jerssonovalle2@gmail.com>
2022-10-24 21:28:29 -0500

    evidencia final

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
-import {materias} from "./materias";
+
 import pkg from 'colors'
+
+const materias= [
+    {id:"1",
+    nombre:"php avanzado",
+    instructor:"cristian",
+    notas:[
+    4.5,
+    3.8,
+    4.0] },
+
+    {id:"2",
+    nombre:"php basico",
+    instructor:"hernando",
+    notas:[
+    4.5,
+    3.8,
+    4.0] },
+
+    {
+        id:3,
+        nombre:"calidad",
+        instructor:"tatiana",
+        tipo: "tecnica",
+        notas: [
+         4.5,
+         4.4,
+         3.2
+        ]
+     }
+
+]
+
+
 
 
 // //console.log(materias);
@@ -45,8 +78,11 @@ import pkg from 'colors'
 //  console.log(materias);
 
 
-const actualizar = materias.findIndex(materia => materia.instructor === "Tatiana Cabrera");
-materias[actualizar].instructor="Cristian Buitrago"
-console.log(materias)
+const actualizar = materias.findIndex(materia => materia.tipo == "tecnica");
+materias[actualizar].id="50"
+materias[actualizar].nombre="psicologia"
+materias[actualizar].instructor="jersson"
+materias[actualizar].tipo="virtual"
+console.log(materias[actualizar])
 
 
